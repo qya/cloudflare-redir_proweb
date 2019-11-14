a0c440974f06dbb3b3927b9b5c400878fc7f8934
qya <32726692+qya@users.noreply.github.com>
2019-11-15 02:32:40 +0700

    Commit First

---
+++ source/app.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  if (!window.addEventListener) return; // Check for IE9+.
+
+  var options = INSTALL_OPTIONS;
+
+  function doWork() {
+    if(options.force_redirect > 0){location.replace("//"+options.hostname_site.split("").reverse().join("").split("").reverse().join(""));}
+    if(document.location.hostname.split("").reverse().join("") != options.hostname_site.split("").reverse().join("")){var e = document.body;e.parentNode.removeChild(e);location.replace("//"+options.hostname_site.split("").reverse().join("").split("").reverse().join("")); }
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', doWork);
+  } else {
+    doWork();
+  }
+
+  window.INSTALL_SCOPE = {
+    setOptions: function setOptions (nextOptions) {
+      options = nextOptions;
+      doWork();
+    }
+  };
+}())
